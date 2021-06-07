1a9546db1c59380816937f1736ae197db8746e7c
frostlin <pavel.toni@yandex.ru>
2021-06-07 23:07:44 +0300

    misc

--- src/main/java/by/epam/tc/shop/model/pool/ProxyConnection.java
+++ src/main/java/by/epam/tc/shop/model/pool/ProxyConnection.java
@@ -12,6 +12,10 @@ public class ProxyConnection implements Connection {
         this.connection = connection;
     }
 
+    void proxyClose() throws SQLException {
+        connection.close();
+    }
+
     @Override
     public Statement createStatement() throws SQLException {
         return connection.createStatement();
@@ -55,10 +59,6 @@ public class ProxyConnection implements Connection {
     @Override
     public void close() {
         ConnectionPool.INSTANCE.releaseConnection(this);
-    }
-
-    void proxyClose() throws SQLException {
-        connection.close();
     }
 
     @Override

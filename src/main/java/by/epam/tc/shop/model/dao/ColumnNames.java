7909b1c9bad4f40621eb2d61cf8e731d3908a3b3
frostlin <pavel.toni@yandex.ru>
2021-06-10 10:29:30 +0300

    added constants for db table columns

---
+++ src/main/java/by/epam/tc/shop/model/dao/ColumnNames.java
@@ -0,0 +1,11 @@
+package by.epam.tc.shop.model.dao;
+
+public class ColumnNames {
+    public static final String USER_ID = "user.id";
+    public static final String EMAIL = "email";
+    public static final String LOGIN = "login";
+    public static final String PASSWORD = "password";
+    public static final String ROLE_NAME = "role.name";
+    public static final String DATE_REGISTERED = "date_registered";
+
+}

417f7a834bf1e2cf8e8b98d047ae660b8284d481
frostlin <pavel.toni@yandex.ru>
2021-06-13 13:16:41 +0300

    added role and login attributes

--- src/main/java/by/epam/tc/shop/controller/SessionAttribute.java
+++ src/main/java/by/epam/tc/shop/controller/SessionAttribute.java
@@ -3,6 +3,8 @@ package by.epam.tc.shop.controller;
 public class SessionAttribute {
     public static final String CURRENT_PAGE = "currentPage";
     public static final String LOCALE = "currentLocale";
+    public static final String ROLE = "role";
+    public static final String LOGIN = "login";
 
     private SessionAttribute(){
     }

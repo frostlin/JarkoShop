42a14afc950722d09bc5bf53f04d9cb036495c76
frostlin <pavel.toni@yandex.ru>
2021-06-13 13:19:00 +0300

    added signin&signup&changeLocale command types

--- src/main/java/by/epam/tc/shop/controller/command/CommandType.java
+++ src/main/java/by/epam/tc/shop/controller/command/CommandType.java
@@ -1,16 +1,41 @@
 package by.epam.tc.shop.controller.command;
 
 import by.epam.tc.shop.controller.*;
-import by.epam.tc.shop.controller.command.impl.SignInCommand;
-import by.epam.tc.shop.controller.command.impl.SignUpCommand;
-import by.epam.tc.shop.controller.command.impl.ToMainPageCommand;
+import by.epam.tc.shop.controller.command.impl.*;
 
 public enum CommandType {
     TO_MAIN{
         {
             this.command = new ToMainPageCommand();
         }
+    },
+    CHANGE_LOCALE{
+        {
+            this.command = new ChangeLocaleCommand();
+        }
+    },
+    TO_SIGN_IN{
+        {
+            this.command = new ToSignInCommand();
+        }
+    },
+    TO_SIGN_UP{
+        {
+            this.command = new ToSignUpCommand();
+        }
+    },
+    SIGN_IN{
+        {
+            this.command = new SignInCommand();
+        }
+    },
+    SIGN_UP{
+        {
+            this.command = new SignUpCommand();
+        }
     };
+
+
 
 
     Command command;

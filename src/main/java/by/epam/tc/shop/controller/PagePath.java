8164e09446acade390d11b5453718d76a09fe64f
frostlin <pavel.toni@yandex.ru>
2021-06-13 13:18:16 +0300

    added signin&signup pages

--- src/main/java/by/epam/tc/shop/controller/PagePath.java
+++ src/main/java/by/epam/tc/shop/controller/PagePath.java
@@ -3,6 +3,7 @@ package by.epam.tc.shop.controller;
 public class PagePath {
         public static final String HOME = "/index.jsp";
         public static final String MAIN = "/pages/main.jsp";
-
+        public static final String SIGN_IN = "/pages/signIn.jsp";
+        public static final String SIGN_UP = "/pages/signUp.jsp";
         private PagePath(){}
 }

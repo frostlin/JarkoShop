05cd28aafcde4207083bb1e00cb6ecba77765597
frostlin <pavel.toni@yandex.ru>
2021-06-16 00:07:05 +0300

    added address amd user tables column names

--- src/main/java/by/epam/tc/shop/model/dao/ColumnNames.java
+++ src/main/java/by/epam/tc/shop/model/dao/ColumnNames.java
@@ -3,9 +3,23 @@ package by.epam.tc.shop.model.dao;
 public class ColumnNames {
     public static final String USER_ID = "user.id";
     public static final String EMAIL = "email";
+    public static final String ROLE_NAME = "role.name";
     public static final String LOGIN = "login";
     public static final String PASSWORD = "password";
-    public static final String ROLE_NAME = "role.name";
+    public static final String SURNAME = "surname";
+    public static final String NAME = "name";
+    public static final String LASTNAME = "lastname";
+    public static final String TELEPHONE = "telephone";
     public static final String DATE_REGISTERED = "date_registered";
+
+    public static final String ADDRESS_ID = "address.id";
+    public static final String ADDRESS_USER_ID = "address.user_id";
+    public static final String COUNTRY = "country";
+    public static final String REGION = "region";
+    public static final String CITY = "city";
+    public static final String STREET = "street";
+    public static final String BUILDING = "building";
+    public static final String APARTMENT = "apartment";
+    public static final String INDEX = "index";
 
 }

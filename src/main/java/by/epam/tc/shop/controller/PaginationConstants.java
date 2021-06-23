b10b2699381b80224d1f5b6c43da97dacc9c142e
frostlin <pavel.toni@yandex.ru>
2021-06-23 23:39:36 +0300

    added pagination constants

---
+++ src/main/java/by/epam/tc/shop/controller/PaginationConstants.java
@@ -0,0 +1,8 @@
+package by.epam.tc.shop.controller;
+
+public class PaginationConstants {
+    public static final int CURRENT_ORDERS_PER_PAGE = 10;
+    public static final int CURRENT_PRODUCTS_PER_PAGE = 3;
+
+    private PaginationConstants(){}
+}
